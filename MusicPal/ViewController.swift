a7b0e7b6bcf705b40f3608487c36e26454f9f6b6
Berke Yağız Sevim <berkeyagizsevim@gmail.com>
2019-12-11 17:43:52 +0300

    Initial Commit

---
+++ MusicPal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPal
+//
+//  Created by Berke Yağız Sevim on 11.12.2019.
+//  Copyright © 2019 Berke Yağız Sevim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
